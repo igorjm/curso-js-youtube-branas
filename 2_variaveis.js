fa6f5bd3fcbbff51d4db1f8ef2cc5510f6a893f6
Igor Melo <igorjmelo4@gmail.com>
2019-10-10 22:45:53 -0300

    first commit

---
+++ 2_variaveis.js
@@ -0,0 +1,18 @@
+var name = "AgileCode";
+var top10 = [1,2,3];
+var $scope = {title: "Javascrit", version: "6"};
+var _person = "John";
+var numberCode = 123;
+var a = 10, b = 9, c = 8;
+var language;
+
+/**  Numbers */
+var nota = 10;
+
+nota.toExponential(2); // 100
+nota.toFixed(2); // 10.00
+nota.toPrecision(1); // 1e+1
+nota.toString(); // "10"
+nota.valueOf(); // 10
+
+/** Strings */
